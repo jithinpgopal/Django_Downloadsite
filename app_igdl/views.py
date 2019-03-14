cbd4c840fab34fd7449c40fe579002baf4b31fd2
Jithin P Gopal <trijithu@gmail.com>
2019-03-14 15:43:10 +0530

    Added git ignore, commented the print implemented for debug

--- app_igdl/views.py
+++ app_igdl/views.py
@@ -8,7 +8,7 @@ def download(request):
 	return render(request, 'app_igdl/download.html')
 
 def image(request):
-	print ("Printing :" + request.POST.get("q"))
+	# print ("Printing :" + request.POST.get("q"))
 	url1 = str(request.POST.get("q"))
 	img = image_download(url1)
 	context = {'img': img}
